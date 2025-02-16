07474b7afa9b64144d3701af11828f0e4b2af65f
Naumikhin Daniil <dan.naumihin@yandex.ru>
2025-02-16 22:58:01 +0300

    Подкорректировал личный кабинет

--- pages/personal_acc/package.py
+++ pages/personal_acc/package.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from data import db_session
 import flask_login
 from flask_login import login_user, login_required
--- pages/personal_acc/personal_page.py
+++ pages/personal_acc/personal_page.py
@@ -24,30 +24,70 @@ def personal_page():
 def edit_personal_data():
     if not flask_login.current_user.is_authenticated:
         return redirect("/register")
+
     curr_id = flask_login.current_user.id
     curr_user = flask_login.current_user
-    form = PersonalDataForm()
-    render_page = lambda msg = None: render_template("edit_personal_data.html", message=msg, form=form)
-    personal_data = None
-    if form.validate_on_submit():
-        with db_session.create_session() as session:
-            personal_data = session.query(PersonalData).filter(PersonalData.user_id == curr_id).first()
-            try: 
-                new_data = False
-                if personal_data is None:
-                    personal_data = PersonalData()
-                    personal_data.user = curr_user
-                    new_data = True
-                personal_data.first_name = form.first_name.data
-                personal_data.last_name = form.last_name.data
-                personal_data.middle_name = form.middle_name.data
-                personal_data.phone_number = form.phone_number.data
-                personal_data.date_of_birth = form.date_of_birth.data
-                if new_data: session.add(personal_data)
-                session.commit()
-                return render_page("Все окей!")
-            except IntegrityError as e:
-                return render_page(f"Что-тт пошло не так: {e}")
+    reference_data = PersonalData()
+    reference_data.user = curr_user
 
-    return render_page()
+    personal_data = reference_data
+    form = PersonalDataForm()
+    render_page = lambda msg = None: render_template(
+        "edit_personal_data.html", message=msg, form=form
+    )
+    def edit_data(session):
+        res = session.query(PersonalData).filter(PersonalData.user_id == curr_id).first()
+        if res is None: return (reference_data, False)
+        return (res, True)
+
+    def commit_dialog(session):
+        session.commit()
+        return render_page("Все окей")
+    
+    if request.method == "GET":
+        with db_session.create_session() as session:
+            try:
+                personal_data, data_exists = edit_data(session)
+                if data_exists: form.process(obj=personal_data)
+            except Exception as e:
+                return render_page(f"Ошибка: {e}")
+            return render_page()
+    
+    if request.method == "POST":
+        if form.validate_on_submit() and form.submit.data:
+            with db_session.create_session() as session:
+                try:
+                    personal_data, data_exists = edit_data(session)
+                    form.populate_obj(personal_data)
+                    if not data_exists: session.add(personal_data)
+                    return commit_dialog(session)
+                except Exception as e:
+                    session.rollback()
+                    return render_page(f"Ошибка: {e}")
+    # curr_id = flask_login.current_user.id
+    # curr_user = flask_login.current_user
+    # form = PersonalDataForm()
+    # render_page = lambda msg = None: render_template("edit_personal_data.html", message=msg, form=form)
+    # personal_data = None
+    # if form.validate_on_submit():
+    #     with db_session.create_session() as session:
+    #         personal_data = session.query(PersonalData).filter(PersonalData.user_id == curr_id).first()
+    #         try: 
+    #             new_data = False
+    #             if personal_data is None:
+    #                 personal_data = PersonalData()
+    #                 personal_data.user = curr_user
+    #                 new_data = True
+    #             personal_data.first_name = form.first_name.data
+    #             personal_data.last_name = form.last_name.data
+    #             personal_data.middle_name = form.middle_name.data
+    #             personal_data.phone_number = form.phone_number.data
+    #             personal_data.date_of_birth = form.date_of_birth.data
+    #             if new_data: session.add(personal_data)
+    #             session.commit()
+    #             return render_page("Все окей!")
+    #         except IntegrityError as e:
+    #             return render_page(f"Что-тт пошло не так: {e}")
+
+    # return render_page()
     
